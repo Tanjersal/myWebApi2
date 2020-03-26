16a58ef56f3045891a4f204f5925929b121a79ac
Fabien Thiombiano <fabienthiombiano@gmail.com>
2020-03-26 15:09:11 +0000

    Init WebAPI primer - essential features

---
+++ Primer/Controllers/PageSizeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Primer.Controllers
+{
+    public class PageSizeController : ApiController
+    {
+        private static string TargetUrl = "http://apress.com";
+
+        /// <summary>
+        /// Returns page size
+        /// </summary>
+        /// <returns></returns>
+        public long GetPageSize()
+        {
+            WebClient client = new WebClient();
+            Stopwatch sw = new Stopwatch();
+
+            byte[] data = client.DownloadData(TargetUrl);
+            Debug.WriteLine("Elpased ms: {0}", sw.ElapsedMilliseconds);
+            return data.LongLength;
+        }
+    }
+}
